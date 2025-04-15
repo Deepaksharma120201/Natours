84ea5dd3729742f6e2e0d5a58b766710edcba6e6
Deepak Sharma <deepaksharma120201@gmail.com>
2025-04-15 22:30:28 +0530

    working on user authentication and authorization

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
 const app = express();
 
 const tourRouter = require("./routes/tourRoutes");
@@ -8,12 +12,41 @@ const userRouter = require("./routes/userRoutes");
 const AppError = require("./appError");
 
 // Creating a middleWare
+app.use(helmet());
 app.use(morgan("dev"));
-app.use(express.json());
+
+// Limit the number of requests from a single IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+app.use(express.json({ limit: "10kb" })); // to parse JSON data
+
+// For security purposes
+// Data sanitization against NoSQL query injection
+// Prevent parameter pollution
+app.use(mongoSanitize());
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
+
 app.use(express.static(`${__dirname}/public`)); // to serve static files
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  // console.log(req.headers);
   next();
 });
 
